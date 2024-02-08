dbf12a614b6fcf6bafd8b6cfeee2014b6877d005
AdrianK47 <kangadrian76@gmail.com>
2024-02-08 14:45:21 -0500

    yipppee

--- ExampleThree.java
+++ ExampleThree.java
@@ -1,3 +1,6 @@
 public class ExampleThree {
-    
+    public static void main (String[] args){
+        System.out.println("Adrian Kang");
+        System.out.println("Poem: \"Quoth the Raven, Nevermore,\"\nProverb: \"Fear not, for I am with you; be not dismayed, for I am your God; I will strengthen you, I will help you, I will uphold you with my righteous right hand.\"");
+    }
 }
--- ExampleTwo.java
+++ ExampleTwo.java
@@ -1,3 +1,8 @@
 public class ExampleTwo {
-    
+    public static void main(String[] args){
+        System.out.println("**************************");
+        System.out.println("*     Sue's Swimwear     *");
+        System.out.println("*SwimWithSue@swimwaer.com*");
+        System.out.println("**************************");
+    }                             
 }
